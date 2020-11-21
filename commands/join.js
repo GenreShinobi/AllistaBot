dda6b10064b94c371c39ca2f79dd5678d0785da2
genreshinobi <genreshinobi@gmail.com>
2020-11-21 10:30:45 -0700

    Updated Join cmd to include role assignment.

--- commands/join.js
+++ commands/join.js
@@ -2,6 +2,12 @@ module.exports = {
 	name: 'join',
 	description: 'Join the A-List Movie Group.',
 	execute(message, args, db) {
+		async function addRole(msg, roleID) {
+			const role = await msg.guild.roles.fetch(roleID);
+			message.member.roles.add(role);
+
+		}
+
 		db.collection('guilds').doc(message.guild.id).collection('users').doc(message.author.id).get()
 			.then((ref) => {
 				if (ref.exists) {
@@ -13,8 +19,11 @@ module.exports = {
 						'userName' : message.author.username,
 						'moviePoints' : 50,
 						'moviesAttended' : 0,
+						'moviesAdded' : 0,
 						'dateJoined' : Date.now(),
 					}).then(function() {
+						addRole(message, '634787377365254157');
+						// message.member.roles.add(role);
 						message.reply('Congratulations! You are now on The A-List!');
 					});
 				}
