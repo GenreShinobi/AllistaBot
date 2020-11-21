05f8700218d3e09ea751a87ea3a137d70f110521
genreshinobi <genreshinobi@gmail.com>
2020-11-21 13:03:01 -0700

    Updated AddMovie to cover more conditions

--- commands/addmovie.js
+++ commands/addmovie.js
@@ -64,6 +64,8 @@ module.exports = {
 		async function asyncCalls() {
 			console.log('Called: asyncCalls()');
 			const eligibility = await getEligibility();
+
+			// Check user Eligibility before proceeding with async addMovie calls.
 			if(eligibility) {
 				const list = await getList();
 				const movie = await getFirstMovie(list);
@@ -81,8 +83,7 @@ module.exports = {
 						{ name: 'Genre:', value: `${movie.Genre}`, inline: true },
 					);
 				message.delete();
-				let mEmbedMsg, lEmbedMsg, cEmbedMsg;
-				mEmbedMsg = await message.channel.send(mEmbed);
+				const mEmbedMsg = await message.channel.send(mEmbed);
 				const filter = m => m.author.id === message.author.id;
 				let userResponse = await awaitResponse(mEmbedMsg, filter);
 				console.log('userResponse was returned: ' + userResponse);
@@ -93,19 +94,28 @@ module.exports = {
 					userResponse.channel.send('TODO: Add to list');
 				}
 				else if (userResponse.content.toUpperCase() == 'NO' || userResponse.content.toUpperCase() == 'N') {
+					// eslint-disable-next-line prefer-const
 					let titleArray = [];
+					// eslint-disable-next-line prefer-const
 					let dateArray = [];
+					// eslint-disable-next-line prefer-const
 					let imdbArray = [];
 
+					// Get the max # of results
 					let max = Object.keys(list.Search).length - 1;
+
+					// If the max # of results is > 10, limit it to the top 10
+					// Reason: formatting becomes an issue with longer lists.
 					if (max > 10) { max = 10; }
 
+					// Generate the arrays that will be used as the value of the inline fields of the Embed
 					for (let i = 0; i < max; i++) {
 						titleArray.push(`[${i + 1}] ${list.Search[i + 1].Title}`);
 						dateArray.push(list.Search[i + 1].Year);
 						imdbArray.push(`[Link](https://www.imdb.com/title/${list.Search[i + 1].imdbID})`);
 					}
 
+					// Construct the List Embed for displaying the fallback list.
 					const lEmbed = new Discord.MessageEmbed()
 						.setColor('#0099ff')
 						.setAuthor('Any of these the correct movie?')
@@ -114,30 +124,61 @@ module.exports = {
 							{ name: 'Date', value: `${dateArray.join('\n')}`, inline: true },
 							{ name: 'IMDB', value: `${imdbArray.join('\n')}`, inline: true },
 						);
-					userResponse.delete();
-					mEmbedMsg.delete();
-					lEmbedMsg = await message.channel.send(lEmbed);
-					userResponse = await awaitResponse(lEmbedMsg, filter);
 
-					if (userResponse.content.toUpperCase() == 'YES' || userResponse.content.toUpperCase() == 'Y') {
-						userResponse.channel.send('Ok, just respond with the ID number.');
-					}
-					else if (userResponse.content.toUpperCase() == 'NO' || userResponse.content.toUpperCase() == 'N') {
-						const cEmbed = new Discord.MessageEmbed()
-							.setColor('#0099ff')
-							.setAuthor('Ok, we can add a custom entry.')
-							.setDescription('Just reply with the full title of the movie.');
+					// Remove the users command line.
+					userResponse.delete();
+
+					// Remove the previous Movie Embed post.
+					mEmbedMsg.delete();
+
+					// Post and get the List embed post.
+					const lEmbedMsg = await message.channel.send(lEmbed);
+
+					waitForResponse: while(true) {
+						// Wait for the users response.
+						userResponse = await awaitResponse(lEmbedMsg, filter);
+
+						// Delete the users command from the chat.
 						userResponse.delete();
-						lEmbedMsg.delete();
-						cEmbedMsg = await userResponse.channel.send(cEmbed);
-						userResponse = await awaitResponse(cEmbedMsg, filter);
-						userResponse.delete();
-						userResponse.channel.send(`**${userResponse.content}** added to alist.`);
-						cEmbedMsg.delete();
+
+						// TODO: Consider moving this processing into a function
+						if (userResponse.content.toUpperCase() == 'YES' || userResponse.content.toUpperCase() == 'Y') {
+							// If the restponse was yes, ask for the ID
+							userResponse.channel.send('Ok, just respond with the ID number.')
+								.then(sentMessage => {
+									sentMessage.delete({ timeout: 5000 });
+								});
+							continue waitForResponse;
+						}
+						else if (userResponse.content > 0 && userResponse.content < max + 1) {
+							// If the response was a valid ID, add the movie.
+							lEmbedMsg.delete();
+							userResponse.channel.send('TODO: Add movie to list.');
+							break;
+						}
+						else if (userResponse.content < 1 || userResponse.content > max + 1) {
+							// If the response was not a valid ID, ask again. 
+							userResponse.channel.send('Sorry, that ID was not valid.');
+							continue waitForResponse;
+						}
+						else if (userResponse.content.toUpperCase() == 'NO' || userResponse.content.toUpperCase() == 'N') {
+							const cEmbed = new Discord.MessageEmbed()
+								.setColor('#0099ff')
+								.setAuthor('Ok, we can add a custom entry.')
+								.setDescription('Just reply with the full title of the movie.');
+							lEmbedMsg.delete();
+							const cEmbedMsg = await userResponse.channel.send(cEmbed);
+							userResponse = await awaitResponse(cEmbedMsg, filter);
+							userResponse.delete();
+							userResponse.channel.send(`**${userResponse.content}** added to alist.`);
+							cEmbedMsg.delete();
+							break;
+						}
 					}
 				}
 			}
 			else {
+				// Response when user is not eligible
 				message.reply('I am sorry but only active A-Listers can add to the A-List.');
 			}
 		}
